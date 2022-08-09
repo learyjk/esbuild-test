349890372a0eda66264499233462909b5f12b0c0
Keegan Leary <leary.keegan@gmail.com>
2022-08-09 12:10:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello");
+let heading = document.querySelector("h1");
+heading.textContent = "This is a new h1";
---
+++ out.js
@@ -0,0 +1,6 @@
+(() => {
+  // index.js
+  console.log("Hello");
+  var heading = document.querySelector("h1");
+  heading.textContent = "This is a new h1";
+})();
