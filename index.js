fe255757efad31118d44a28422137680686ef3e7
Keegan Leary <leary.keegan@gmail.com>
2022-08-09 12:31:29 -0700

    basic esbuild watch and minified build

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log("Hello");
+import confetti from "canvas-confetti";
+
+console.log("Removed out.js");
+console.log("Is it watching?");
 let heading = document.querySelector("h1");
 heading.textContent = "This is a new h1";
+
+var myCanvas = document.createElement("canvas");
+myCanvas.style.width = "100vw";
+myCanvas.style.height = "100vh";
+document.body.appendChild(myCanvas);
+
+var myConfetti = confetti.create(myCanvas, {
+  resize: true,
+  useWorker: true,
+});
+myConfetti({
+  particleCount: 300,
+  spread: 160,
+  // any other options from the global
+  // confetti function
+});
--- out.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  // index.js
-  console.log("Hello");
-  var heading = document.querySelector("h1");
-  heading.textContent = "This is a new h1";
-})();
